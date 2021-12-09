ca91fd17cc05212463e26ec4aff2c39775d63dcd
M-Vaidehi-R <m_ramesh@fanshaweonline.ca>
2021-12-09 09:36:37 -0500

    added images to the game with PIL

--- components/quiz_array.py
+++ components/quiz_array.py
@@ -1,4 +1,3 @@
 quiz_total = 0
 
 characters = ["Winnie the Pooh", "Christopher Robin", "Kanga", "Tigger","rabbit", "pigglet", "Eeyore", "Roo" "Owl"]
-images = ["pooh.jpg", "robin.jpg", "kanga.jpg", "trigger.jpg", "rabbit.jpg", "pigglet.jpg", "eeyore.jpg", "roo.jpg", "owl.jpg"]
